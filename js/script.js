fc32a9fef73105595b076fc2bab41dd3002b854f
Xasstel <Xasstel@gmail.com>
2025-05-04 02:18:10 +0300

    1.2 done + update readme

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function aboutClick(){
+    window.location.href = "https://google.com";
+}
+
+function downloadClick(){
+    window.location.href = "https://google.com";
+}
+
+function patchesClick(){
+    window.location.href = "https://google.com";
+}
